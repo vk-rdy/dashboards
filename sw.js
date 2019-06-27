4a20248a9bc36331374cd94f39d4d87ec24b50e9
Vamshi K <vkarla@transunion.com>
2019-06-27 15:42:28 -0500

    sw.js changes

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ self.addEventListener('install', function(e) {
     caches.open(cacheName).then(function(cache) {
       console.log('[Service Worker] Caching all: app shell and content');
       return cache.addAll(appShellFiles);
-    })
+    });
   );
 });
 
